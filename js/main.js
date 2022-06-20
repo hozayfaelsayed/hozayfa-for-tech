a33a32a2cedb798080d7319b927fa41bbb7ec72c
hozayfaelsayed <hozayfaelsayed88@gmail.com>
2022-06-20 20:07:10 +0200

    some changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 
-// function startLoader() {
-//     const loader = document.querySelector(".spinner");
+ function startLoader() {
+    const loader = document.querySelector(".spinner");
   
-//     setTimeout(function() {
-//       loader.style.left = "0";
-//     }, 2500);
+     setTimeout(function() {
+       loader.style.left = "0";
+     }, 2500);
   
-//     window.addEventListener("DOMContentLoaded", function(event) {
-//       setTimeout(function() {
-//         loader.style.left = "100%";
-//         document.body.classList.remove("spinner");
-//       }, 3000);
-//     });
-//   }
+     window.addEventListener("DOMContentLoaded", function(event) {
+       setTimeout(function() {
+         loader.style.left = "100%";
+         document.body.classList.remove("spinner");
+       }, 3000);
+    });
+   }
   
-//   startLoader();
+   startLoader();
   
