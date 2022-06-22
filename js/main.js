cef1659c4084d6169d70fb5f5dff6052fd362cf2
hozayfaelsayed <hozayfaelsayed88@gmail.com>
2022-06-23 01:12:48 +0200

    pro 71

--- js/main.js
+++ js/main.js
@@ -15,4 +15,24 @@
    }
   
    startLoader();
+
+  //  scroll top
+  let span = document.querySelector(".top");
+
+window.onscroll = function () {
+   console.log(this.scrollY);
+   if (this.scrollY >= 1000) {
+    span.classList.add("show");
+   } else {
+    span.classList.remove("show");
+  }
+  
+};
+
+span.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
   
